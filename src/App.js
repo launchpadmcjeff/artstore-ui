347c8566d5e18d1301f4d7bf7141c8191d656d83
Jeff McKenzie <LaunchpadMcJeff@gmail.com>
2019-12-22 15:33:25 -0800

    saving progress and moving to better laptop

--- src/App.js
+++ src/App.js
@@ -10,6 +10,12 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <button onClick={getProducts}>
+          getProducts
+</button>
+        <button onClick={postOrders}>
+          postOrders
+</button>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -19,8 +25,46 @@ function App() {
           Learn React
         </a>
       </header>
+
     </div>
   );
+}
+
+async function getProducts(e) {
+  e.preventDefault();
+  const response = await fetch('http://localhost:8080/artstore/products', { headers: { 'Content-Type': 'application/json' } });
+  const myJson = await response.json();
+  console.log(JSON.stringify(myJson));
+
+}
+
+// Example POST method implementation:
+async function postOrders(e) {
+  e.preventDefault();
+  try {
+    const data = await postData('http://localhost:8080/artstore/orders', {});
+    console.log(JSON.stringify(data)); // JSON-string from `response.json()` call
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function postData(url = '', data = {}) {
+  // Default options are marked with *
+  const response = await fetch(url, {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    mode: 'cors', // no-cors, *cors, same-origin
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: 'same-origin', // include, *same-origin, omit
+    headers: {
+      'Content-Type': 'application/json'
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: 'follow', // manual, *follow, error
+    referrerPolicy: 'no-referrer', // no-referrer, *client
+    body: JSON.stringify(data) // body data type must match "Content-Type" header
+  });
+  return await response.json(); // parses JSON response into native JavaScript objects
 }
 
 export default App;
