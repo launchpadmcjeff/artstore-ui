cb281ccfd31e9fa4671ee26dba6c33b7078f11cf
Jeff McKenzie <LaunchPadMcJeff@gmail.com>
2019-12-27 12:18:39 -0800

    remainder of shopping status component extract

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     console.log('App constructor');
-    // this.state = { input: "" };
   }
 
 
@@ -19,11 +18,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <header>
-          {/* <div className="Shopping-status">
-            <FontAwesomeIcon icon={faShoppingCart} className="p-1" color="#fff" />
-
-          </div> */}
-          <ShoppingStatus/>
+          <ShoppingStatus />
           <h1>The Artstore</h1>
         </header>
 
@@ -51,55 +46,55 @@ class App extends React.Component {
 
 
   getProducts = async (e) => {
-  e.preventDefault();
-  const response = await fetch('http://localhost:8080/artstore/rest/products', { headers: { 'Content-Type': 'application/json' } });
-  const myJson = await response.json();
-  console.log(JSON.stringify(myJson));
+    e.preventDefault();
+    const response = await fetch('http://localhost:8080/artstore/rest/products', { headers: { 'Content-Type': 'application/json' } });
+    const myJson = await response.json();
+    console.log(JSON.stringify(myJson));
 
-}
- getOrders99 = async (e) => {
-  e.preventDefault();
-  const response = await fetch('http://localhost:8080/artstore/rest/orders/99', { headers: { 'Content-Type': 'application/json' } });
-  const myJson = await response.json();
-  console.log(JSON.stringify(myJson));
-
-}
- getOrders = async(e) => {
-  e.preventDefault();
-  const response = await fetch('http://localhost:8080/artstore/rest/orders', { headers: { 'Content-Type': 'application/json' } });
-  const myJson = await response.json();
-  console.log(JSON.stringify(myJson));
-
-}
-
-// Example POST method implementation:
- postOrders = async (e) => {
-  e.preventDefault();
-  try {
-    const data = await this.postData('http://localhost:8080/artstore/rest/orders', {});
-    console.log(JSON.stringify(data)); // JSON-string from `response.json()` call
-  } catch (error) {
-    console.error(error);
   }
-}
+  getOrders99 = async (e) => {
+    e.preventDefault();
+    const response = await fetch('http://localhost:8080/artstore/rest/orders/99', { headers: { 'Content-Type': 'application/json' } });
+    const myJson = await response.json();
+    console.log(JSON.stringify(myJson));
 
- postData = async (url = '', data = {}) => {
-  // Default options are marked with *
-  const response = await fetch(url, {
-    method: 'POST', // *GET, POST, PUT, DELETE, etc.
-    mode: 'cors', // no-cors, *cors, same-origin
-    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: 'same-origin', // include, *same-origin, omit
-    headers: {
-      'Content-Type': 'application/json'
-      // 'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    redirect: 'follow', // manual, *follow, error
-    referrerPolicy: 'no-referrer', // no-referrer, *client
-    body: JSON.stringify(data) // body data type must match "Content-Type" header
-  });
-  return await response.json(); // parses JSON response into native JavaScript objects
-}
+  }
+  getOrders = async (e) => {
+    e.preventDefault();
+    const response = await fetch('http://localhost:8080/artstore/rest/orders', { headers: { 'Content-Type': 'application/json' } });
+    const myJson = await response.json();
+    console.log(JSON.stringify(myJson));
+
+  }
+
+  // Example POST method implementation:
+  postOrders = async (e) => {
+    e.preventDefault();
+    try {
+      const data = await this.postData('http://localhost:8080/artstore/rest/orders', {});
+      console.log(JSON.stringify(data)); // JSON-string from `response.json()` call
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  postData = async (url = '', data = {}) => {
+    // Default options are marked with *
+    const response = await fetch(url, {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      mode: 'cors', // no-cors, *cors, same-origin
+      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: 'same-origin', // include, *same-origin, omit
+      headers: {
+        'Content-Type': 'application/json'
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: 'follow', // manual, *follow, error
+      referrerPolicy: 'no-referrer', // no-referrer, *client
+      body: JSON.stringify(data) // body data type must match "Content-Type" header
+    });
+    return await response.json(); // parses JSON response into native JavaScript objects
+  }
 }
 
 export default App;
