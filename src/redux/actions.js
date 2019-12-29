8d5b25b058e265745a4280735d6aebe7b62b71a9
Jeff McKenzie <LaunchPadMcJeff@gmail.com>
2019-12-29 15:46:50 -0800

    foo and bar action creators

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,8 +1,22 @@
-import { ADD_TO_CART } from "./actionTypes";
+import { ADD_TO_CART, FOO, BAR } from "./actionTypes";
 
 export const addToCart = item => ({
   type: ADD_TO_CART,
   payload: {
     item
+  }
+});
+
+export const foo = foo => ({
+  type: FOO,
+  payload: {
+    foo
+  }
+});
+
+export const bar = bar => ({
+  type: BAR,
+  payload: {
+    bar
   }
 });
