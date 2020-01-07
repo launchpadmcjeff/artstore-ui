6433b6a6382827d97b23bdec08b1dbd76fec471f
Jeff McKenzie <LaunchPadMcJeff@gmail.com>
2020-01-07 15:30:13 -0800

    start of implementation

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import * as serviceWorker from './serviceWorker';
 import store from "./redux/store";
 import Root from './components/Root'
+import { submitOrder } from './redux/actions'
 
 
 ReactDOM.render(<Root store={store} />, document.getElementById('root'))
@@ -12,3 +13,16 @@ ReactDOM.render(<Root store={store} />, document.getElementById('root'))
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+const foo = () => {
+    console.log('hello: ' + JSON.stringify(store.getState().shipping));
+    // store.dispatch(submitOrder());
+
+};
+
+setInterval(foo, 600000);
+
+const bar = () => {
+    console.log(store.getState());
+}
+const unsubscribe = store.subscribe(bar);
