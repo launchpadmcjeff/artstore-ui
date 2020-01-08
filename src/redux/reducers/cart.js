4220197f1fb24cb23fbc0df30395b26c76eaf5f5
Jeff McKenzie <LaunchPadMcJeff@gmail.com>
2020-01-08 13:05:08 -0800

    clear cart after successful submit

--- src/redux/reducers/cart.js
+++ src/redux/reducers/cart.js
@@ -1,27 +1,53 @@
-import { ADD_TO_CART, REMOVE_FROM_CART } from "../actionTypes";
+import {
+  ADD_TO_CART, REMOVE_FROM_CART,
+  SUBMIT_ORDER_BEGIN,
+  SUBMIT_ORDER_SUCCESS,
+  SUBMIT_ORDER_FAILURE
+} from "../actionTypes";
+
 const initialState = {
   itemCount: 0,
   lineItems: [],
   subtotal: 0,
   tax: 0,
   total: 0,
-  discountCodes: []
+  discountCodes: [],
+  loading: false,
+  error: null
 };
 
 const retSums = (lineItems) => {
   const subtotal = lineItems.map(i => i.price).reduce((r, i) => r + i, 0);
   const tax = Math.round(subtotal * .06);
   const total = subtotal + tax;
-  return {subtotal, tax, total};
+  return { subtotal, tax, total };
 }
 
 const cart = (state = initialState, action) => {
   switch (action.type) {
+
+    case SUBMIT_ORDER_BEGIN:
+      return {
+        ...state,
+        loading: true,
+        error: null
+      };
+
+    case SUBMIT_ORDER_SUCCESS:
+      return Object.assign({}, initialState);
+
+    case SUBMIT_ORDER_FAILURE:
+      return {
+        ...state,
+        loading: false
+      };
+
+
     case REMOVE_FROM_CART: {
       const { id } = action.payload;
       const lineItems = state.lineItems.filter(i => i.id !== id);
       const retSumz = retSums(lineItems);
-      return Object.assign({}, state, { itemCount: lineItems.length}, retSumz, { lineItems: lineItems });
+      return Object.assign({}, state, { itemCount: lineItems.length }, retSumz, { lineItems: lineItems });
       ;
     }
     case ADD_TO_CART: {
@@ -36,7 +62,7 @@ const cart = (state = initialState, action) => {
       const lineItems = [...state.lineItems, lineItem];
       const retSumz = retSums(lineItems);
 
-      return Object.assign({}, state, { itemCount: lineItems.length}, retSumz, { lineItems });
+      return Object.assign({}, state, { itemCount: lineItems.length }, retSumz, { lineItems });
     }
     default: {
       return state;
